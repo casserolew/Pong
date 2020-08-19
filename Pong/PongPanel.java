fd41322f5d0250083f4131981b2d6d4b8706c405
Cassidy Wenmoth <c.wenmoth@gmail.com>
2020-08-20 09:04:33 +1200

    All Classes have been created

--- Pong/PongPanel.java
+++ Pong/PongPanel.java
@@ -1,58 +1,81 @@
+import java.awt.BasicStroke;
  import java.awt.Color;
-   import java.awt.event.ActionEvent;
-   import java.awt.event.ActionListener;
-   import java.awt.event.KeyEvent;
-   import java.awt.event.KeyListener;
-   import javax.swing.Timer;
-   import javax.swing.JPanel;
-   import java.awt.Graphics;
-   import java.awt.Graphics2D;
-   import java.awt.BasicStroke;
-   import java.awt.Stroke;
-   import java.awt.Graphics2D;
-   
-   
-   public class PongPanel extends JPanel implements ActionListener, KeyListener {
-       private final static Color BACKGROUND_COLOUR = Color.black;
-       private final static int TIMER_DELAY = 5;
-       
-       
+ import java.awt.Graphics;
+ import java.awt.Graphics2D;
+ import java.awt.Stroke;
+ import java.awt.event.ActionEvent;
+ import java.awt.event.ActionListener;
+ import java.awt.event.KeyEvent;
+ import java.awt.event.KeyListener;
+  
+ import javax.swing.JPanel;
+ import javax.swing.Timer;
+  
+ public class PongPanel extends JPanel implements ActionListener, KeyListener {
+      
+      private final static Color BACKGROUND_COLOUR = Color.BLACK;
+      private final static int TIMER_DELAY = 5;
+      
+      GameState gameState = GameState.Initialising;
+      
+      Ball ball;
+      Paddle paddle1, paddle2;
+      
       public PongPanel() {
           setBackground(BACKGROUND_COLOUR);
-          Timer timer = new Timer(TIMER_DELAY, null);
+          Timer timer = new Timer(TIMER_DELAY,this);
           timer.start();
       }
       
-      @Override
-      public void paintComponent(Graphics g) {
-          super.paintComponent(g);
-          paintDottedLine(g);
- 
+      public void createObjects() {
+          ball = new Ball(getWidth(), getHeight());
+          paddle1 = new Paddle(Player.One, getWidth(), getHeight());
+          paddle2 = new Paddle(Player.Two, getWidth(), getHeight());
+      }
+      
+      private void update() {
+          switch(gameState) {
+              case Initialising: {
+                  createObjects();
+                  gameState = GameState.Playing;
+                  break;
+              }
+              case Playing: {
+                  break;
+              }
+              case GameOver: {
+                  break;
+              }
+          }
       }
       
       private void paintDottedLine(Graphics g) {
-          Graphics2D g2d = (Graphics2D) g.create();
+         Graphics2D g2d = (Graphics2D) g.create();
              Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
              g2d.setStroke(dashed);
              g2d.setPaint(Color.WHITE);
              g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
              g2d.dispose();
-     }
-      private void update() {
-          
       }
       
-      
+      private void paintSprite(Graphics g, Sprite sprite) {
+          g.setColor(sprite.getColour());
+          g.fillRect(sprite.getXPosition(), sprite.getYPosition(), sprite.getWidth(), sprite.getHeight());
+      }
+  
       @Override
       public void keyPressed(KeyEvent event) {
+          
       }
   
       @Override
       public void keyReleased(KeyEvent event) {
+          
       }
   
       @Override
       public void keyTyped(KeyEvent event) {
+          
       }
       
       @Override
@@ -61,5 +84,15 @@
           repaint();
       }
       
+      @Override
+      public void paintComponent(Graphics g) {
+          super.paintComponent(g);
+          paintDottedLine(g);
+          if(gameState != GameState.Initialising) {
+              paintSprite(g, ball);
+              paintSprite(g, paddle1);
+              paintSprite(g, paddle2);
   
+          }
+      }
  }
